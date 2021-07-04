7463259fed1445d57e0aa54205ebd9ca26b8289d
Zalan Farkas <zalan.farkas.16@gmail.com>
2021-07-04 16:27:32 +0100

    Add example

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,30 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonButton, IonContent, IonHeader, IonPage, IonTitle, IonToolbar, useIonLoading } from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Home.css';
 
 const Home: React.FC = () => {
+  const [present, dismiss] = useIonLoading();
+
+  // does NOT dismiss
+  const plain = () => {
+    present();
+    dismiss();
+  }
+
+  // does NOT dismiss
+  const withAwait = async () => {
+    await present();
+    await dismiss();
+  }
+
+  // DOES dismiss if timeout is 50 ms or more but not all the time
+  const withTimeout = () => {
+    present();
+    setTimeout(() => dismiss(), 50);
+  }
+
+
+
   return (
     <IonPage>
       <IonHeader>
@@ -16,7 +38,9 @@ const Home: React.FC = () => {
             <IonTitle size="large">Blank</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <ExploreContainer />
+        <IonButton onClick={plain}>Plain</IonButton>
+        <IonButton onClick={withAwait}>withAwait</IonButton>
+        <IonButton onClick={withTimeout}>withTimeout</IonButton>
       </IonContent>
     </IonPage>
   );
